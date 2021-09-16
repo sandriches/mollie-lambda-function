f60ef68f8af13789dd38234236fcac7275a85755
Richard Corke <richard.corke@hotmail.co.uk>
2021-09-16 13:04:40 +0200

    fix handler export

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import AWS from 'aws-sdk';
 import { createMollieClient } from '@mollie/api-client';
 const mollieClient = createMollieClient({ apiKey: 'test_ayMsEe8EhVxcAysPwCPe35WfzQWnWE' });
 
-exports.handler = (async () => {
+exports.handler = async () => {
     // Methods for the Payments API
     let methods = await mollieClient.methods.all();
 
     // Methods for the Orders API
     methods = await mollieClient.methods.all({ resource: 'orders' });
     console.log(methods);
-})();
+};
