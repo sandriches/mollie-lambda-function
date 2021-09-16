d93cf82e8f37df9439f502a416f2da2d048b654a
Richard Corke <rich@Richards-MacBook-Pro.local>
2021-09-16 11:44:01 +0200

    Add base project

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const api_client_1 = require("@mollie/api-client");
+const mollieClient = (0, api_client_1.createMollieClient)({ apiKey: 'test_ayMsEe8EhVxcAysPwCPe35WfzQWnWE' });
+(async () => {
+    // Methods for the Payments API
+    let methods = await mollieClient.methods.all();
+    // Methods for the Orders API
+    methods = await mollieClient.methods.all({ resource: 'orders' });
+    console.log(methods);
+})();
